82ac02bc38a4c41bd7115c50ad8ba5073a755846
Aryan Shah <shaharyan2508@gmail.com>
2026-07-18 01:02:31 +0530

    feat: redesign customer UI and fix restaurant flow

--- src/data/Categories.js
+++ src/data/Categories.js
@@ -1,50 +1,7 @@
-// import pizza from "../assets/images/categories/pizza.jfif";
-// import burger from "../assets/images/categories/burger.jfif";
-// import biryani from "../assets/images/categories/biryani.jfif";
-// import chinese from "../assets/images/categories/chinese.jfif";
-// import southIndian from "../assets/images/categories/south-indian.jfif";
-// import desserts from "../assets/images/categories/desserts.jfif";
-
-// const categories = [
-//   {
-//     id: 1,
-//     name: "Pizza",
-//     image: pizza,
-//   },
-//   {
-//     id: 2,
-//     name: "Burger",
-//     image: burger,
-//   },
-//   {
-//     id: 3,
-//     name: "Biryani",
-//     image: biryani,
-//   },
-//   {
-//     id: 4,
-//     name: "Chinese",
-//     image: chinese,
-//   },
-//   {
-//     id: 5,
-//     name: "South Indian",
-//     image: southIndian,
-//   },
-//   {
-//     id: 6,
-//     name: "Desserts",
-//     image: desserts,
-//   },
-// ];
-
-// export default categories;
-
-
 import pizza from "../assets/images/categories/pizza.jfif";
 import burger from "../assets/images/categories/burger.jfif";
 import biryani from "../assets/images/categories/biryani.jfif";
-import chinese from "../assets/images/categories/Chinese.jfif";
+import chinese from "../assets/images/categories/chinese.jfif";
 import southIndian from "../assets/images/categories/south-Indian.jfif";
 import desserts from "../assets/images/categories/desserts.jfif";
 
